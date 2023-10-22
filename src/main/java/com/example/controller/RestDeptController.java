a7e61e31a916c577b1bf97503fea0b1991ec7964
big1 <big1@example.com>
2023-10-22 15:43:05 +0900

    정리

--- src/main/java/com/example/controller/RestDeptController.java
+++ src/main/java/com/example/controller/RestDeptController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.mapper.DeptMapper;
 import com.example.model.Dept;
